dbecca68f69fc2c462f733eb6ec0fa68adc748cf
Marco Carrozzo <ozeta86@gmail.com>
2023-10-24 23:53:18 +0200

    train on single city. add samples from '39 until today

--- read_lotto.py
+++ read_lotto.py
@@ -6,51 +6,105 @@ from keras.layers import LSTM, Bidirectional, Dropout, Dense
 from tensorflow import keras
 from keras.optimizers import Adam
 
-df = pd.read_csv('storico01-oggi.txt', sep='\t', header=None, na_filter=False)
-df.drop([0], axis=1, inplace=True)
-# create a set of strings using the first column
-cities = set(df[1].astype(str))
 
-cities_df = {}
+# import Sequential from keras
 
-for city in cities:
-    # select the rows where the city column is equal to the current city
-    subset = df[df[1] == city]
-    # print the first 10 rows of the subset
-    trunc_sub = df.drop([1], axis=1)
-    cities_df[city] = trunc_sub
-    # print(subset.head(10))
-    scaler = StandardScaler().fit(cities_df[city])
-    transformed_dataset = scaler.transform(cities_df[city])
-    transformed_df = pd.DataFrame(data=transformed_dataset, index=cities_df[city].index)
-    print(transformed_df.head())
-    number_of_rows = cities_df[city].values.shape[0]
-    window_length = 7
-    number_of_features = cities_df[city].values.shape[1]
+def rnn(units, dense_units, input_shape, number_of_features, X, y, epochs, batch_size, learning_rate):
+    """
+        Default settings used in medium article:
+        units=240
+        dense_units=59
+        epochs=300
+        batch_size=100
+        learning_rate=0.0001
+    """
+    model = Sequential()
+    model.add(Bidirectional(LSTM(units, input_shape = input_shape, return_sequences = True)))
+    model.add(Dropout(0.2))
+    model.add(Bidirectional(LSTM(units, input_shape = input_shape, return_sequences = True)))
+    model.add(Dropout(0.2))
+    model.add(Bidirectional(LSTM(units, input_shape = input_shape, return_sequences = True)))
+    model.add(Bidirectional(LSTM(units, input_shape = input_shape, return_sequences = False)))
+    model.add(Dense(dense_units))
+    model.add(Dense(number_of_features))
+
+    model.compile(optimizer=Adam(learning_rate=learning_rate), loss ='mse', metrics=['accuracy'])
+    model.fit(x=X, y=y, batch_size=batch_size, epochs=epochs, verbose=2)
     
+    return model
+
+def build_cities_dict(df: pd.DataFrame, cities: set) -> dict:
+    """
+    Build a dictionary of dataframes, one for each city
+    """
+    cities_dict = {}
+    for city in cities:
+        # select the rows where the city column is equal to the current city
+        subset = df[df[1] == city]
+        # remove the city column
+        trunc_sub = df.drop([1], axis=1)
+        cities_dict[city] = trunc_sub
+    return cities_dict
+        
+def main():
+    print("Hello World!")
+    df = pd.read_csv('storico.txt', sep='\t', header=None, na_filter=False)
+    # drop the dates
+    df.drop([0], axis=1, inplace=True)
+    # create a set containing all the cities of Lotto, using the first column
+    cities = set(df[1].astype(str))
+
+    cities_dict = build_cities_dict(df, cities)
+
+    city = "NA"
+
+    # normalize the data into format [0,1]
+    scaler = StandardScaler().fit(cities_dict[city])
+    transformed_dataset = scaler.transform(cities_dict[city])
+    transformed_df = pd.DataFrame(data=transformed_dataset, index=cities_dict[city].index)
+    print(transformed_df.head())
+
+    number_of_rows = cities_dict["NA"].values.shape[0]
+    window_length = 7
+    # number of balls for a "ruota"
+    number_of_features = cities_dict["NA"].values.shape[1]
+
+    # create the training set
     X = np.empty([ number_of_rows - window_length, window_length, number_of_features], dtype=float)
     y = np.empty([ number_of_rows - window_length, number_of_features], dtype=float)
     for i in range(0, number_of_rows-window_length):
         X[i] = transformed_df.iloc[i : i+window_length, 0 : number_of_features]
         y[i] = transformed_df.iloc[i+window_length : i+window_length+1, 0 : number_of_features]
-    print(X.shape)
-    print(y.shape)
-    # import Sequential from keras
-    model = Sequential()
-    model.add(Bidirectional(LSTM(240, input_shape = (window_length, number_of_features), return_sequences = True)))
-    model.add(Dropout(0.2))
-    model.add(Bidirectional(LSTM(240, input_shape = (window_length, number_of_features), return_sequences = True)))
-    model.add(Dropout(0.2))
-    model.add(Bidirectional(LSTM(240, input_shape = (window_length, number_of_features), return_sequences = True)))
-    model.add(Bidirectional(LSTM(240, input_shape = (window_length, number_of_features), return_sequences = False)))
-    model.add(Dense(59))
-    model.add(Dense(number_of_features))
+    print(f"X shape: {X.shape}; y shape: {y.shape}")
+    
+    print("Training the model...")
+    model = rnn(units=240, 
+                dense_units=59, 
+                input_shape=(window_length, number_of_features), 
+                number_of_features=number_of_features, 
+                X=X, 
+                y=y, 
+                epochs=4, 
+                batch_size=100, 
+                learning_rate=0.0001)
 
-    model.compile(optimizer=Adam(learning_rate=0.0001), loss ='mse', metrics=['accuracy'])
-    model.fit(x=X, y=y, batch_size=100, epochs=300, verbose=2)
-    to_predict = df.tail(8)
-    print(f"{city} -> to_predict {to_predict}")
-# head = cities_df["NA"].iloc[::100000, :]
+    print("Predicting the next lottery numbers...")
 
-# cities_df["NA"].head(1000).plot(x=1,y=2, style='o', figsize=(20, 18)).figure.savefig('plot.png')
-print("OK")
+    city_df = cities_dict["NA"]
+
+    to_predict = city_df.tail(8)
+    to_predict.drop([to_predict.index[-1]],axis=0, inplace=True)
+
+
+    to_predict = np.array(to_predict)
+    scaled_to_predict = scaler.transform(to_predict)
+
+    y_pred = model.predict(np.array([scaled_to_predict]))
+    print("The predicted numbers in the last lottery game are:", scaler.inverse_transform(y_pred).astype(int)[0])
+
+    prediction = city_df.tail(1)
+    prediction = np.array(prediction)
+    print("The actual numbers in the last lottery game were:", prediction[0])
+    print("END")
+if __name__ == "__main__":        
+    main()
---
+++ readme.md
@@ -0,0 +1,17 @@
+# readme
+```
+https://medium.com/@polanitzer/how-to-guess-accurately-3-lottery-numbers-out-of-6-using-lstm-model-e148d1c632d6
+https://github.com/TimeSerieser/How-to-Guess-Accurately-3-Lottery-Numbers-Out-of-6-using-LSTM/blob/main/Israeli%20Lottery.ipynb
+```
+
+```bash
+https://www.lotto-italia.it/lotto/estratti-ruote
+curl -vL https://www.igt.it/STORICO_ESTRAZIONI_LOTTO/storico.zip -O 
+unzip storico.txt
+```
+
+```bash
+pipenv install
+pipenv shell
+python3 read_lotto.py
+```
